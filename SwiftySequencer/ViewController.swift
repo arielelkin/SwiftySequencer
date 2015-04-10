ec846a26f6181aadabe8130c1bb7e8f6ca61dc65
Ariel Elkin <arielelkin@gmail.com>
2015-04-10 19:13:29 +0200

    initial commit

---
+++ SwiftySequencer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftySequencer
+//
+//  Created by Ariel Elkin on 10/04/2015.
+//  Copyright (c) 2015 Ariel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
