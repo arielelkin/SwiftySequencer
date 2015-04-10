26c54b75b930e285f229272448339c594c6d6299
Ariel Elkin <arielelkin@gmail.com>
2015-04-10 19:40:16 +0200

    add sequencer code in swift

--- SwiftySequencer/ViewController.swift
+++ SwiftySequencer/ViewController.swift
@@ -10,19 +10,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        var controller = AEAudioController(audioDescription: AEAudioController.nonInterleavedFloatStereoAudioDescription())
-
+        var audioManager = AudioManager()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+}
 
+class AudioManager {
+    let audioController: AEAudioController
+    var woodblockSoundChannel: AESequencerChannel
+    var crickSoundChannel: AESequencerChannel
+
+
+    init() {
+
+        //Init AEAudioController:
+        audioController = AEAudioController(audioDescription: AEAudioController.nonInterleavedFloatStereoAudioDescription())
+
+        var audioControllerStartError: NSError?
+        var success = audioController.start(&audioControllerStartError)
+        if (!success) {
+            println("audioControllerStartError: \(audioControllerStartError)")
+        }
+
+        //Setup woodblock channel:
+        var woodblockURL = NSBundle.mainBundle().URLForResource("woodblock", withExtension: "caf")
+
+        var woodblockSoundSequence = AESequencerChannelSequence()
+        woodblockSoundSequence.addBeat(AESequencerBeat(onset: 0))
+        woodblockSoundSequence.addBeat(AESequencerBeat(onset: 0.25))
+        woodblockSoundSequence.addBeat(AESequencerBeat(onset: 0.50))
+        woodblockSoundSequence.addBeat(AESequencerBeat(onset: 0.75))
+
+        woodblockSoundChannel = AESequencerChannel(
+            audioFileAt: woodblockURL,
+            audioController: audioController,
+            withSequence: woodblockSoundSequence,
+            numberOfFullBeatsPerMeasure: 4,
+            atBPM: 120)
+
+
+        //Setup crick channel:
+        var crickURL = NSBundle.mainBundle().URLForResource("crick", withExtension: "caf")
+
+        var crickSoundSequence = AESequencerChannelSequence()
+        crickSoundSequence.addBeat(AESequencerBeat(onset: 1.0 / 4 + 2.0 / 16, velocity:0.25))
+        crickSoundSequence.addBeat(AESequencerBeat(onset: 2.0 / 4 + 1.0 / 16, velocity:0.5))
+        crickSoundSequence.addBeat(AESequencerBeat(onset: 2.0 / 4 + 2.0 / 16, velocity:0.125))
+        crickSoundSequence.addBeat(AESequencerBeat(onset: 2.0 / 4 + 3.0 / 16, velocity:0.5))
+        crickSoundSequence.addBeat(AESequencerBeat(onset: 3.0 / 4 + 1.0 / 8, velocity:0.5))
+
+        crickSoundChannel  = AESequencerChannel(
+            audioFileAt: crickURL,
+            audioController: audioController,
+            withSequence: crickSoundSequence,
+            numberOfFullBeatsPerMeasure: 4,
+            atBPM: 120)
+
+
+
+        //Add sequencer channels to audio controller:
+        audioController.addChannels([crickSoundChannel])
+        crickSoundChannel.sequenceIsPlaying = true
+
+
+        audioController.addChannels([woodblockSoundChannel])
+        woodblockSoundChannel.sequenceIsPlaying = true
+
+
+    }
 
 }
 
