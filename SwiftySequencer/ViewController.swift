58232f712138e19f655a44eaff1b209b6f77e2ad
Ariel Elkin <arielelkin@gmail.com>
2015-04-11 15:10:30 +0200

    make ui programmatically

--- SwiftySequencer/ViewController.swift
+++ SwiftySequencer/ViewController.swift
@@ -10,18 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var audioManager = AudioManager()
+
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+    }
 
-        var audioManager = AudioManager()
+    func reverbSizeSliderMoved(sender:UISlider) {
+        audioManager.reverbDryWet = sender.value
+        println("reverb dry wet: \(audioManager.reverbDryWet)")
+    }
+
+    func playbackRateSliderMoved(sender:UISlider) {
+        audioManager.playbackRateFilterRate = sender.value
+        println("playback rate: \(audioManager.playbackRateFilterRate)")
     }
 
 }
 
 class AudioManager {
-    let audioController: AEAudioController
-    var woodblockSoundChannel: AESequencerChannel
-    var crickSoundChannel: AESequencerChannel
+    private let audioController: AEAudioController
+    private var woodblockSoundChannel: AESequencerChannel
+    private var crickSoundChannel: AESequencerChannel
+
+    private var reverb: AEAudioUnitFilter
+    private var playbackRateFilter: AEAudioUnitFilter
+
+
+    var reverbDryWet:Float = 0.0 {
+        willSet {
+            var status = AudioUnitSetParameter(
+                reverb.audioUnit,
+                OSType(kReverb2Param_DryWetMix),
+                OSType(kAudioUnitScope_Global),
+                0,
+                AudioUnitParameterValue(newValue),
+                0);
+        }
+    }
+
+    var playbackRateFilterRate:Float = 0.0 {
+        willSet {
+            AudioUnitSetParameter(
+                playbackRateFilter.audioUnit,
+                OSType(kVarispeedParam_PlaybackRate),
+                OSType(kAudioUnitScope_Global),
+                0,
+                AudioUnitParameterValue(newValue),
+                0);
+        }
+    }
 
 
     init() {
@@ -80,7 +118,127 @@ class AudioManager {
         woodblockSoundChannel.sequenceIsPlaying = true
 
 
+        /** Effects **/
+        var errorEffectSetup: NSError?
+
+        //Setup Reverb:
+        var reverbDescription = AEAudioComponentDescriptionMake(
+            OSType(kAudioUnitManufacturer_Apple),
+            OSType(kAudioUnitType_Effect),
+            OSType(kAudioUnitSubType_Reverb2))
+
+        reverb = AEAudioUnitFilter(
+            componentDescription: reverbDescription,
+            audioController: audioController,
+            error: &errorEffectSetup)
+
+
+        if (errorEffectSetup != nil) {
+            println("Error setting up reverb: \(errorEffectSetup)");
+        }
+
+        audioController.addFilter(reverb, toChannel: crickSoundChannel)
+
+
+        //Setup playback rate filter:
+        var pitchFilterDescription = AEAudioComponentDescriptionMake(
+            OSType(kAudioUnitManufacturer_Apple),
+            OSType(kAudioUnitType_FormatConverter),
+            OSType(kAudioUnitSubType_Varispeed))
+
+        playbackRateFilter = AEAudioUnitFilter(
+            componentDescription: pitchFilterDescription,
+            audioController: audioController,
+            error:&errorEffectSetup)
+
+
+        if errorEffectSetup != nil {
+            println("Error setting up pitch filter: \(errorEffectSetup)");
+        }
+
+        audioController.addFilter(playbackRateFilter, toChannel: woodblockSoundChannel)
+
     }
 
 }
 
+extension ViewController {
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor.whiteColor()
+
+
+        var reverbLabel = UILabel()
+        reverbLabel.text = "Reverb dry/wet:"
+        reverbLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        view.addSubview(reverbLabel)
+
+
+        var reverbSlider = UISlider()
+        reverbSlider.minimumValue = 0.0
+        reverbSlider.maximumValue = 100.0
+        reverbSlider.addTarget(self, action: "reverbSizeSliderMoved:", forControlEvents: UIControlEvents.ValueChanged)
+        reverbSlider.setTranslatesAutoresizingMaskIntoConstraints(false)
+        view.addSubview(reverbSlider)
+
+
+        var playbackRateLabel = UILabel()
+        playbackRateLabel.text = "Playback Rate:"
+        playbackRateLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        view.addSubview(playbackRateLabel)
+
+        var playbackRateSlider = UISlider()
+        playbackRateSlider.minimumValue = -5.0
+        playbackRateSlider.maximumValue = 5.0
+        playbackRateSlider.addTarget(self, action: "playbackRateSliderMoved:", forControlEvents: UIControlEvents.ValueChanged)
+        playbackRateSlider.setTranslatesAutoresizingMaskIntoConstraints(false)
+        view.addSubview(playbackRateSlider)
+
+
+
+
+        var viewsDict = ["reverbLabel": reverbLabel,
+        "reverbSlider": reverbSlider,
+        "playbackRateLabel": playbackRateLabel,
+        "playbackRateSlider": playbackRateSlider]
+
+        var metrics = ["vSpacing": 20]
+
+        var horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-[reverbLabel]",
+            options: NSLayoutFormatOptions.allZeros,
+            metrics: nil,
+            views: viewsDict)
+        view.addConstraints(horizontalConstraints)
+
+        horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-[reverbSlider]-|",
+            options: NSLayoutFormatOptions.allZeros,
+            metrics: nil,
+            views: viewsDict)
+        view.addConstraints(horizontalConstraints)
+
+        horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-[playbackRateLabel]",
+            options: NSLayoutFormatOptions.allZeros,
+            metrics: nil,
+            views: viewsDict)
+        view.addConstraints(horizontalConstraints)
+
+        horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-[playbackRateSlider]-|",
+            options: NSLayoutFormatOptions.allZeros,
+            metrics: nil,
+            views: viewsDict)
+        view.addConstraints(horizontalConstraints)
+
+        var verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-30-[reverbLabel]-vSpacing-[reverbSlider]-vSpacing-[playbackRateLabel]-vSpacing-[playbackRateSlider]",
+            options: NSLayoutFormatOptions.allZeros,
+            metrics: metrics,
+            views: viewsDict)
+        view.addConstraints(verticalConstraints)
+
+    }
+}
